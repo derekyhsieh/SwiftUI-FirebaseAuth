7c8508ca8d070d18c1559e9b28abe2dbb866b0f2
DerekHsiehDev <devderekhsieh@gmail.com>
2023-01-07 23:00:50 -0800

    Initial Commit

---
+++ SwiftUI-Auth/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUI-Auth
+//
+//  Created by Derek Hsieh on 1/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Auth/SwiftUI_AuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AuthApp.swift
+//  SwiftUI-Auth
+//
+//  Created by Derek Hsieh on 1/7/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
